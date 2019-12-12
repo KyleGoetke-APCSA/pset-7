a5d0647a5dd0cc3684d070a1c791f15a7b6aa698
Kyle Goetke <kgoetke@ucvts.tec.nj.us>
2019-12-12 13:54:10 -0500

    Added cases for application menus

--- src/com/apcsa/controller/Application.java
+++ src/com/apcsa/controller/Application.java
@@ -78,7 +78,7 @@ public class Application {
                 	while (validLogin) {
                         switch (getSelectionRoot()) {
                         case RTCHANGEPWD: System.out.println("\nroot change password\n"); break;
-                        case RTRESETDB: PowerSchool.reset(); System.out.println("\nroot reset database\n"); break;
+                        case RTRESETDB: /*PowerSchool.reset();*/ System.out.println("\nroot reset database\n"); break;
                         case RTSHUTDOWN: System.out.println("\nroot shut down\n"); break;
                         case RTLOGOUT: validLogin = false; in.nextLine(); break;
                         default: System.out.println("\nInvalid selection.\n"); break;
@@ -88,7 +88,12 @@ public class Application {
                 	boolean validLogin = true;
                 	while (validLogin) {
                         switch (getSelectionAdministrator()) {
-                        case ADBYFAC: System.out.println("\nCASE\n"); break;
+                        case ADBYFAC: System.out.println("\nview by faculty\n"); break;
+                        case ADBYDEP: System.out.println("\nview by dept\n"); break;
+                        case ADBYENROLL: System.out.println("\nview enrollment\n"); break;
+                        case ADBYGRADE: System.out.println("\nview by grade\n"); break;
+                        case ADBYCOURSE: System.out.println("\nview by course\n"); break;
+                        case ADCHANGEPWD: System.out.println("\nadmin change password\n"); break;
                         case ADLOGOUT: validLogin = false; in.nextLine(); break;
                         default: System.out.println("\nInvalid selection.\n"); break;
                         }
@@ -97,7 +102,11 @@ public class Application {
                 	boolean validLogin = true;
                 	while (validLogin) {
                         switch (getSelectionTeacher()) {
-                        case TCBYCOURSE: System.out.println("\nCASE\n"); break;
+                        case TCBYCOURSE: System.out.println("\nview enrollment by course\n"); break;
+                        case TCNEWASGN: System.out.println("\nadd assignment\n"); break;
+                        case TCDLTASGN: System.out.println("\ndelete assignment\n"); break;
+                        case TCNEWGRD: System.out.println("\nenter grade\n"); break;
+                        case TCCHANGEPWD: System.out.println("\nteacher change password\n"); break;
                         case TCLOGOUT: validLogin = false; in.nextLine(); break;
                         default: System.out.println("\nInvalid selection.\n"); break;
                         }
@@ -106,7 +115,9 @@ public class Application {
                 	boolean validLogin = true;
                 	while (validLogin) {
                         switch (getSelectionStudent()) {
-                        case STVIEWGRD: System.out.println("\nCASE\n"); break;
+                        case STVIEWGRD: System.out.println("\nview course grades\n"); break;
+                        case STBYCOURSE: System.out.println("\nview asgn grades by course\n"); break;
+                        case STCHANGEPWD: System.out.println("\nstudent change password\n"); break;
                         case STLOGOUT: validLogin = false; in.nextLine(); break;
                         default: System.out.println("\nInvalid selection.\n"); break;
                         }
@@ -134,7 +145,7 @@ public class Application {
     public int getSelectionRoot() {
         System.out.println("[1] Reset user password.");
         System.out.println("[2] Factory reset database.");
-        System.out.println("[3] Logout");
+        System.out.println("[3] Logout.");
         System.out.println("[4] Shutdown.");
 
         if (in.hasNextInt()) {
@@ -146,11 +157,14 @@ public class Application {
     }
 
     public int getSelectionAdministrator() {
-        System.out.println("[1] View balance");
-        System.out.println("[2] Deposit money");
-        System.out.println("[3] Withdraw money");
-        System.out.println("[4] Transfer money");
-        System.out.println("[5] Logout");
+    	System.out.println("Hello again, [USER FIRST NAME]!\n");
+    	System.out.println("[1] View faculty.");
+        System.out.println("[2] View faculty by department.");
+        System.out.println("[3] View student enrollment.");
+        System.out.println("[4] View student enrollment by grade.");
+        System.out.println("[5] View student enrollment by course.");
+        System.out.println("[6] Change password.");
+        System.out.println("[7] Logout.");
 
         if (in.hasNextInt()) {
         	return in.nextInt();
@@ -161,11 +175,12 @@ public class Application {
     }
 
     public int getSelectionTeacher() {
-        System.out.println("[1] View balance");
-        System.out.println("[2] Deposit money");
-        System.out.println("[3] Withdraw money");
-        System.out.println("[4] Transfer money");
-        System.out.println("[5] Logout");
+        System.out.println("[1] View enrollment by course.");
+        System.out.println("[2] Add assignment.");
+        System.out.println("[3] Delete assignment.");
+        System.out.println("[4] Enter grade.");
+        System.out.println("[5] Change password.");
+        System.out.println("[6] Logout.");
 
         if (in.hasNextInt()) {
         	return in.nextInt();
@@ -176,11 +191,10 @@ public class Application {
     }
 
     public int getSelectionStudent() {
-        System.out.println("[1] View balance");
-        System.out.println("[2] Deposit money");
-        System.out.println("[3] Withdraw money");
-        System.out.println("[4] Transfer money");
-        System.out.println("[5] Logout");
+        System.out.println("[1] View course grades.");
+        System.out.println("[2] View assignment grades by course.");
+        System.out.println("[3] Change password.");
+        System.out.println("[4] Logout.");
 
         if (in.hasNextInt()) {
         	return in.nextInt();
